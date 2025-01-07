c2575d6135c3cb25ade675b24df354ccd91812cc
wangyoulang <1759923654@qq.com>
2025-01-07 22:11:25 +0800

    第六单：人脸签到管理系统

---
+++ 接单/第六单：人脸签到管理系统/face-check-in/src/main/java/org/com/facecheckin/FaceCheckInApplication.java
@@ -0,0 +1,13 @@
+package org.com.facecheckin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceCheckInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceCheckInApplication.class, args);
+    }
+
+}
---
+++ 接单/第六单：人脸签到管理系统/face-check-in/src/test/java/org/com/facecheckin/FaceCheckInApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.facecheckin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceCheckInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
